65ec513467b80512098efd644232b666e55d9599
azize <hamza.mehri@esprit.tn>
2015-10-15 17:56:45 +0100

    change salutation message

--- ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
+++ ParseStarterProject/src/main/java/com/parse/starter/MainActivity.java
@@ -23,7 +23,7 @@ public class MainActivity extends ActionBarActivity {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
 
-    System.out.println("Goodbye");
+    System.out.println("Morchkana");
 
 
     ParseAnalytics.trackAppOpenedInBackground(getIntent());
